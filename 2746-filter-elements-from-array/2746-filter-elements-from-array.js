f524810197fa55499f3ac66c8a62812e02ea9ee6
Patchanapong Meetam <165882679+patchanaab@users.noreply.github.com>
2024-08-06 14:52:04 +0700

    Time: 55 ms (46.8%), Space: 50 MB (5.41%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    // return arr.filter(fn); // use filter method
+    const res = [];
+    for(const i in arr){
+        if(fn(arr[i],Number(i))){
+            res.push(arr[i]);
+        }
+    }
+    return res
+};
